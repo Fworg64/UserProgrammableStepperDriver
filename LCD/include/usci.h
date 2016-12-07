26a0f406081c10e880708351780c1fd43716167b
fworg64 <fworg64@gmail.com>
2016-12-07 17:15:08 -0600

    Removed old files

--- LCD/include/usci.h
+++
@@ -1,8 +0,0 @@
-#ifndef _USCI_H_
-#define _USCI_H_
-
-void USART_transmit (unsigned char);
-void USART_init (unsigned int ubrr);//CALL TO INIT WITH 103 FOR 9600 BAUD
-void USART_transmit_array (unsigned char*, unsigned char);
-
-#endif
