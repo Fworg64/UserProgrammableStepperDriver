973843e14a81a97b2842caeb3192f29d79c78565
Marc <dashneptune@gmail.com>
2016-12-08 17:59:39 -0600

    Finally I can see clearly now the rain is gone

--- src/main.c
+++ src/main.c
@@ -15,7 +15,6 @@
 #include "menu.h"
 #include "timer.h"
 #include "eeprom.h"
-
 #include "keypad.h"
 
 #define STEPPER_NUMBER_OF_STEPS		8
@@ -45,14 +44,9 @@ const char stepperarray[STEPPER_NUMBER_OF_STEPS]=
 0x03 << BIT_PLACE, 0x01 << BIT_PLACE, 0x09 << BIT_PLACE, 0x08 << BIT_PLACE
 };
 
-//unsigned char direction = DIRECTION_UP;
-//unsigned char running = RUNNING_ON;
-//unsigned char numberofrots = 0;
 unsigned char ms = 0;
 
 void port_init (void);
-
-<<<<<<< HEAD
 
 struct stepper {
 	unsigned long ctr;
@@ -69,72 +63,47 @@ void set_rpm (struct stepper *stepper, unsigned int rpm);
 
 // portd0 is interrupt 0
 //
-=======
 //buffer for text stuff
-char mycars[32] = {'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0'};
+char mycars[33] = {'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0'};
 char tempindex=0;
 char getanotherkey=1;
->>>>>>> origin/master
 
 int main (void)
 {
 	USART_init (103); // 9600 baud
-<<<<<<< HEAD
 	timer_init ();
 	port_init ();
-=======
-	//eeprom_redundant_write (startup);
-	ms_timer_init ();
-	//timer_init ();
-	//port_init ();
->>>>>>> origin/master
-	lcd_init (USART_transmit_array);
-	set_rpm (&stepper_1, 20);
+	set_rpm (&stepper_1, 60);
 	stepper_1.enabled = RUNNING_OFF;
+	lcd_init (USART_transmit_array);
 	lcd_reset ();
 	lcd_set_backlight (8);
 	lcd_set_contrast(50);
-
-	keypad_init();
-
+	//keypad_init();
 	sei ();	// enable interrupts
-<<<<<<< HEAD:src/main.c
-	//timer_start ();
-	ms_timer_start ();
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
 	timer_start ();
->>>>>>> stepperfun:src/main2.c
-	while (1)
-	{
-=======
-	//timer_start ();
-	ms_timer_start ();
 	while (3)
 	{
-        if (getanotherkey) pollKeys(ms); //call this guy everyframe
-        if (wasKeyPressed() && !wasKeyReleased() && getanotherkey) // a key was pressed, but not yet released
-        {
-            mycars[tempindex] = getKey();
-            getanotherkey=0; //this key has been got
-            if (++tempindex>30) tempindex =0;
-            lcd_send_string(mycars);
-        }
-        if (wasKeyPressed() && !wasKeyReleased() && !getanotherkey) // key pressed, need to check if it was released.
-        {
-            pollKeys(ms);
-            if (wasKeyReleased()) //key released, ok to get another
-            {
-                clearKey();
-                getanotherkey =1;
-            }
-        }
-<<<<<<< HEAD:src/main.c
->>>>>>> e3f6a26a054f7a5e083bdff2e22b88fce85c9b88
-=======
->>>>>>> origin/master
->>>>>>> stepperfun:src/main2.c
+	/*
+		if (getanotherkey) pollKeys(ms); //call this guy everyframe
+
+		if (wasKeyPressed() && !wasKeyReleased() && getanotherkey) // a key was pressed, but not yet released
+		{
+		    mycars[tempindex] = getKey();
+		    getanotherkey=0; //this key has been got
+		    if (++tempindex>30) tempindex =0;
+		    lcd_send_string(mycars);
+		}
+		if (wasKeyPressed() && !wasKeyReleased() && !getanotherkey) // key pressed, need to check if it was released.
+		{
+		    pollKeys(ms);
+		    if (wasKeyReleased()) //key released, ok to get another
+		    {
+		        clearKey();
+		        getanotherkey =1;
+		    }
+		}
+	*/
 	}
 	return 0;
 }
@@ -149,7 +118,6 @@ ISR (TIMER1_COMPA_vect)
 			PORTF = stepperarray[stepper_1.index];
 			if (stepper_1.direction == DIRECTION_UP)
 			{
-<<<<<<< HEAD
 				stepper_1.index++;
 				stepper_1.number_of_rots++;
 				if (stepper_1.index >= STEPPER_NUMBER_OF_STEPS)
@@ -166,24 +134,6 @@ ISR (TIMER1_COMPA_vect)
 				stepper_1.index--;
 				stepper_1.number_of_rots++;
 			}
-=======
-				stepperctr = 0;
-			}
-		}
-		else
-		{
-			if (stepperctr == 0)
-			{
-				stepperctr = STEPPER_NUMBER_OF_STEPS;
-			}
-			stepperctr--;
-			numberofrots++;
-		}
-		if (numberofrots == STEPS_PER_ROT){
-			running = RUNNING_OFF;
-			numberofrots = 0;
-			deadband = 0;
->>>>>>> origin/master
 		}
 	}
 	if (ms_sub_timer++ >= MS_SUB_MAX){
