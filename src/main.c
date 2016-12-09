8707e431c26d163518c087e00b847fb914f00f7f
Austin F. Oltmanns <fworg64@gmail.com>
2016-12-09 16:28:12 -0600

    Added a bunch of stuff
    It might work

--- src/main.c
+++ src/main.c
@@ -38,6 +38,11 @@
 
 // clock freq is 250KHz/2500
 
+#define DEFINE define
+#DEFINE MAINMENU =0;
+#DEFINE RUNNINGMENU =1;
+#DEFINE SETTINGMENU =2;
+
 const char stepperarray[STEPPER_NUMBER_OF_STEPS]=
 {
 0x0C << BIT_PLACE, 0x04 << BIT_PLACE, 0x06 << BIT_PLACE, 0x02 << BIT_PLACE,
@@ -46,15 +51,20 @@ const char stepperarray[STEPPER_NUMBER_OF_STEPS]=
 
 unsigned char ms = 0;
 
+unsigned char screen =0;
+char mainmenustring[] =    "1.GO XX.XX 3.FFW2.SET RPM  4.FBW";
+char runningmenustring[] = "1.STOP                          ";
+char settingsmenustring[]= "Curr: XX.XX RPM New :   .  RPM  ";
+
 void port_init (void);
 
 struct stepper {
-	unsigned long ctr;
-	unsigned long cmp;
-	unsigned char enabled;
-	unsigned char direction;
-	unsigned long number_of_rots;
-	unsigned char index;
+	unsigned long ctr; // 0
+	unsigned long cmp; // set by set rpm
+	unsigned char enabled; // enable
+	unsigned char direction; //DIRECTION_UP OR DONWN
+	unsigned long number_of_rots; //0 //ACTUALLY NUMBER OF STEPS
+	unsigned char index; //SET TO ZERO
 };
 
 struct stepper stepper_1 = {.ctr = 0, .enabled = 0, .direction = DIRECTION_UP, .number_of_rots = 0, .index = 0};
@@ -64,9 +74,17 @@ void set_rpm (struct stepper *stepper, unsigned int rpm);
 // portd0 is interrupt 0
 //
 //buffer for text stuff
-char mycars[33] = {'\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0'};
-char tempindex=0;
+char inputcar;
 char getanotherkey=1;
+
+//eeprom and rpm stuff
+struct eeprom_struct eeprom_rpm_high = {.startaddress = 25, .data = 10, .number_of_redundancy = 5};
+struct eeprom_struct eeprom_rpm_low  = {.startaddress = 30, .data = 10, .number_of_redundancy = 5}; 
+int eeprpm_setup();
+void eeprpm_write(int rpm2write);
+char EEPROM_ERROR=0;
+int rpm; //initialized from eeprpm_setup
+char rpmdisplaychars[5];
 
 int main (void)
 {
@@ -79,20 +97,45 @@ int main (void)
 	lcd_reset ();
 	lcd_set_backlight (8);
 	lcd_set_contrast(50);
-	//keypad_init();
+	keypad_init();
 	sei ();	// enable interrupts
 	timer_start ();
+	rpm = epprpm_setup(); //setup rpm in memory from value potentially stored in eemprom. If no valid value is found a default one is written and loaded.
+	set_rpm(&stepper_1, rpm)
+	
+	rpmdisplaychars[0] = rpm/1000 + '0';
+	rpmdisplaychars[1] = rpm/100 -rpmdisplaychars[0] +'0';
+	rpmdisplaychars[2] = '.';
+	rpmdisplaychars[3] = rpm/10 - rpmdisplaychars[0] - rpmdisplaycharsp[1] + '0';
+	rpmdisplaychars[4] = rpm - rpmdisplaychars[0] - rpmdisplaycharsp[1] - rpmdisplaychars[3] + '0';
+	
 	while (3)
 	{
-	/*
 		if (getanotherkey) pollKeys(ms); //call this guy everyframe
 
+		//get input
 		if (wasKeyPressed() && !wasKeyReleased() && getanotherkey) // a key was pressed, but not yet released
 		{
-		    mycars[tempindex] = getKey();
-		    getanotherkey=0; //this key has been got
-		    if (++tempindex>30) tempindex =0;
-		    lcd_send_string(mycars);
+		    inputcar = getKey();
+		    getanotherkey=0; 
+			
+			//this key has been got
+			switch (screen)
+			{
+				case MAINMENU:
+					if (inputcar = '1') 
+					{
+						stepper_1.enabled = 1;
+						screen = RUNNINGMENU;
+					}
+					break;
+				case RUNNINGMENU:
+					if (inputcar = '1') stepper_1.enabled =0;
+					break;
+			}
+			
+		    //if (++tempindex>30) tempindex =0;
+		    //lcd_send_string(mycars);
 		}
 		if (wasKeyPressed() && !wasKeyReleased() && !getanotherkey) // key pressed, need to check if it was released.
 		{
@@ -103,7 +146,17 @@ int main (void)
 		        getanotherkey =1;
 		    }
 		}
-	*/
+		
+		//display output;
+		switch (screen)
+			{
+				case MAINMENU:
+					lcd_send_string(mainmenustring);
+					break;
+				case RUNNINGMENU:
+					lcd_send_string(runningmenustring);
+					break;
+			}
 	}
 	return 0;
 }
@@ -175,6 +228,37 @@ void set_rpm (struct stepper *stepper, unsigned int rpm){
 		stepper->cmp = (STEPPER_DRIVE_FREQ/ STEPS_PER_ROT) * 60;
 		stepper->cmp /= rpm;
 	}
+}
+
+int eeprpm_setup()
+{
+	//initial dummy struct
+	EEPROM_ERROR = eeprom_redundant_read(&eeprom_rpm_high); //check here for eeprom errors
+	EEPROM_ERROR = eeprom_redundant_read(&eeprom_rpm_low); //also here
+	//check for sanity of values
+	if (eeprom_rpm_high.data > 99 || eeprom_rpm_low.data >99) EEPROM_ERROR = 1;//ERROR, try to write better values
+	if (EEPROM_ERROR)
+	{
+		//set error flag
+		eeprom_rpm_high.data = 10;
+		eeprom_rpm_low.data = 10;
+		eeprom_redundant_write(eeprom_rpm_low);
+		eeprom_redundant_write(eeprom_rpm_high);
+	}
+	
+	//convert high low chars to int
+	return eeprom_rpm_high.data * 100 + eeprom_rpm_low.data;
+
+}
+
+void eeprpm_write(int rpm2write)
+{
+	//split rpm2write into high and low
+	eeprom_rpm_high.data = char(rpm2write/100);
+	eeprom_rpm_low.data = char (rpm2write - eeprom_rpm_high.data*100);
+	eeprom_redundant_write(eeprom_rpm_high);
+	eeprom_redundant_write(eeprom_rpm_low);
+	
 }
 
 
