637f2ce688933c21f353884ab2c9c6212d6f7386
fworg64 <fworg64@gmail.com>
2016-12-14 13:07:00 -0600

    Added code for stepper struct

--- include/stepper.h
+++ include/stepper.h
@@ -1,3 +1,15 @@
 #ifndef _STEPPER_H_
 #define _STEPPER_H_
 
+typedef struct mystepper
+{
+    volatile char* stepperport;
+    volatile char* dirpinmask, faultpinmask, steppinmask;
+    int togglecomparetime, dir, internaltimer;
+    char enable;
+} T_STEPPER;
+
+//T_STEPPER stepper_maker(volatile char* stepperport, volatile char* dirpinmask, volatile char* faultpinmask, volatile char* steppinmask, int togglecomparetime, int dir, int internaltimer);
+
+int RPMtotoggletime(int rpm); //toggle time in ms
+int toggletimetoRPM(int toggletime); //toggletime in ms
--- src/stepper.c
+++ src/stepper.c
@@ -1,1 +1,11 @@
 //stepper.c
+
+int RPMtotoggletime(int rpm)
+{
+    return (4.8*1000)/rpm;
+}
+
+int toggletimetoRPM(int toggletime)
+{
+    return RPMtotoggletime(toggletime); //they undo each other
+}
