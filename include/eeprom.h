ce02b01280a769c81b4648c555354b9995bec673
Marc <dashneptune@gmail.com>
2016-12-19 11:20:11 -0600

    changed up eeprom and made new branch for wheel application

--- include/eeprom.h
+++ include/eeprom.h
@@ -10,8 +10,21 @@ struct eeprom_struct {
 	unsigned int startaddress;
 	unsigned char data;
 	unsigned char number_of_redundancy;
+	unsigned char err;
 };
 
-int eeprom_redundant_read (struct eeprom_struct*);
+
+struct eeprom_16_bit {
+	struct eeprom_struct high_byte;
+	struct eeprom_struct low_byte;
+	unsigned int data;
+	unsigned char err;
+};
+
+void eeprom_redundant_read (struct eeprom_struct *);
 void eeprom_redundant_write (struct eeprom_struct);
+void eeprom_16_bit_write (struct eeprom_16_bit *, int to_write);
+void eeprom_16_bit_read (struct eeprom_16_bit *);
+void eeprom_16_bit_init (struct eeprom_16_bit * to_setup, unsigned int start_address, unsigned int number_of_redundancy);
+
 #endif
